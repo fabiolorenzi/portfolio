40be8bfa215593c5fb0ff55f8b2b84f755cddd89
fabiolorenzi <fabiolorenzi@gmail.com>
2021-03-03 14:37:09 +0000

    implementing burger menu

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -2,8 +2,27 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 function BurgerMenu() {
+    const [open, setOpen] = useState(false);
+
+    const burgerOpener = e => {
+        e.preventDefault();
+        setOpen(!open);
+    };
     return(
-        <div className="burgerMenu"></div>
+        <div className="burgerMenu">
+            <div className="burgerButton" id={open ? "burgerOpen" : ""} onClick={burgerOpener}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className="hidden_menu" id={open ? "hidden_menu_open" : "hidden_menu_closed"}>
+                <nav>
+                    <NavLink to="/" className="menu2" activeClassName="sel2" exact>HOME</NavLink>
+                    <NavLink to="/projects" className="menu2" activeClassName="sel2">PROJECTS</NavLink>
+                    <NavLink to="/contacts" className="menu2" activeClassName="sel2">CONTACTS</NavLink>
+                </nav>
+            </div>
+        </div>
     );
 };
 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function Menu() {
+
     return(
         <div className="menu">
             <nav>
